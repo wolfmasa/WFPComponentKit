0b049bc9d628b3fec47a5c25caf67f76d4c5b0ae
Masa Jobara <wolf.masa@gmail.com>
2015-05-20 15:54:59 +0900

    Initial Commit

---
+++ WFPCompnentKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WFPCompnentKit
+//
+//  Created by JobaraMasashi on 2015/05/20.
+//  Copyright (c) 2015年 ProjectJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WFPCompnentKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WFPCompnentKit
+//
+//  Created by JobaraMasashi on 2015/05/20.
+//  Copyright (c) 2015年 ProjectJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
